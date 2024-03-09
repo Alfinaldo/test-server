41e0a209d32b3e461fa4241f84db013d1a5eeed6
Alfinaldo <alvin.aldo22@gmail.com>
2024-03-10 02:51:41 +0700

    first commit

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+// Membuat instance dari aplikasi Express
+const app = express();
+
+// Menentukan port server
+const port = 3000;
+
+// Mengatur route untuk merespons permintaan GET ke halaman utama
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Menjalankan server dan mendengarkan port yang ditentukan
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
